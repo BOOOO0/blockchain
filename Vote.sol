ce75cd0797b7d7489c7eee2ba82857c80b713837
Buyoung Chung <sunarol@naver.com>
2024-04-14 21:23:03 +0900

    feat: 블록체인 투표 앱 컨트랙트 생성

---
+++ Vote.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+// 솔리디티 버전 명시
+// 상단에 라이센스 Identifier를 명시해야 한다.
+
+contract Vote {
+
+    // constructure (후보의 정보와 몇 표를 받았는지 구조체로 저장할 것)
+
+    // variable
+
+    // mapping
+
+    // event (블록체인에서 무엇을 했는지 브로드캐스팅 하는 목적이라고 함)
+
+    // modifier (finishVote를 할 수 있는 주체를 확인하기 위해 사용할 것)
+
+    //constructor
+
+    // candidator
+    function addCandidator() public{
+
+    }
+
+    // voting
+    function Voting() public {
+
+    }
+
+    // finish vote
+    function finishVote() public {
+
+    }
+}
